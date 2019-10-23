2e4d942209b3ccea832dff808bdd27811b7f8794
victor gonzalez <victorjosegonzalez9@gmail.com>
2019-10-23 02:36:08 -0400

    prueba de un menu

--- agroquimica/src/agroquimica/Redneu.java
+++ agroquimica/src/agroquimica/Redneu.java
@@ -39,10 +39,11 @@ public class Redneu extends javax.swing.JFrame {
         jTextField1 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         evaluar = new javax.swing.JButton();
+        imagen = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jList1 = new javax.swing.JList<>();
-        imagen = new javax.swing.JLabel();
         salir = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -53,17 +54,19 @@ public class Redneu extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Arial", 3, 12)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("selecciona una imagen:");
-        panelprincipal.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(75, 204, -1, -1));
-        panelprincipal.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 202, 202, -1));
+        panelprincipal.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 230, -1, -1));
+        panelprincipal.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 230, 202, -1));
 
+        jButton1.setBackground(new java.awt.Color(255, 255, 255));
         jButton1.setText("ua");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
-        panelprincipal.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(428, 201, 27, -1));
+        panelprincipal.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 230, 27, -1));
 
+        evaluar.setBackground(new java.awt.Color(255, 255, 255));
         evaluar.setFont(new java.awt.Font("Arial", 3, 11)); // NOI18N
         evaluar.setText("evaluar");
         evaluar.addActionListener(new java.awt.event.ActionListener() {
@@ -71,7 +74,13 @@ public class Redneu extends javax.swing.JFrame {
                 evaluarActionPerformed(evt);
             }
         });
-        panelprincipal.add(evaluar, new org.netbeans.lib.awtextra.AbsoluteConstraints(262, 235, -1, -1));
+        panelprincipal.add(evaluar, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 260, -1, -1));
+
+        imagen.setBackground(new java.awt.Color(255, 255, 255));
+        imagen.setForeground(new java.awt.Color(255, 255, 255));
+        imagen.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255), 2));
+        imagen.setOpaque(true);
+        panelprincipal.add(imagen, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 40, 202, 172));
 
         jList1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -85,40 +94,44 @@ public class Redneu extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(jList1);
 
-        panelprincipal.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 270, 439, 89));
-
-        imagen.setBackground(new java.awt.Color(255, 255, 255));
-        imagen.setForeground(new java.awt.Color(255, 255, 255));
-        imagen.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255), 2));
-        panelprincipal.add(imagen, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 11, 202, 172));
+        panelprincipal.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 290, 439, 89));
 
         salir.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        salir.setForeground(new java.awt.Color(255, 255, 255));
         salir.setText("X");
         salir.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                salirMouseClicked(evt);
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                salirMousePressed(evt);
             }
         });
-        panelprincipal.add(salir, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 0, 17, -1));
+        panelprincipal.add(salir, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 0, 20, -1));
+
+        jLabel2.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel2.setOpaque(true);
+        jLabel2.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                jLabel2MouseDragged(evt);
+            }
+        });
+        jLabel2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jLabel2MousePressed(evt);
+            }
+        });
+        panelprincipal.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 590, 30));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panelprincipal, javax.swing.GroupLayout.PREFERRED_SIZE, 590, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(panelprincipal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panelprincipal, javax.swing.GroupLayout.PREFERRED_SIZE, 380, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(panelprincipal, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void salirMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_salirMouseClicked
-        System.exit(0);
-    }//GEN-LAST:event_salirMouseClicked
 
     private void jList1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jList1KeyPressed
         if (evt.getKeyCode() == java.awt.event.KeyEvent.VK_ENTER) {
@@ -180,6 +193,20 @@ public class Redneu extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void salirMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_salirMousePressed
+        //System.exit(0);
+        this.dispose();
+    }//GEN-LAST:event_salirMousePressed
+    private int x,y;
+    private void jLabel2MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MousePressed
+        x=evt.getX();
+        y=evt.getY();
+    }//GEN-LAST:event_jLabel2MousePressed
+
+    private void jLabel2MouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseDragged
+        this.setLocation(evt.getXOnScreen()-x, evt.getYOnScreen()-y);
+    }//GEN-LAST:event_jLabel2MouseDragged
+
     /**
      * @param args the command line arguments
      */
@@ -225,6 +252,7 @@ public class Redneu extends javax.swing.JFrame {
     private javax.swing.JLabel imagen;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JList<String> jList1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField jTextField1;
